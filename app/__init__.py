f9a0fb9ec1372b96ebcc3122f1cf83ef4058325f
Lucas Polo <pradopolo@Wgmail.com>
2018-06-18 19:23:40 -0300

    Criado modelos básicos da aplicação (tabelas)
    close #3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 from .controllers import default
