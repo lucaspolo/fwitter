a2823ad8bf8e12ecc44e45aa6ef67c9f486ca5b8
Lucas Polo <pradopolo@Wgmail.com>
2018-06-18 19:44:20 -0300

    Criado gerenciador da aplicação e migrações
    close #5

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from .controllers import default
